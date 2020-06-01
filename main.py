06709a2c720f5b2e6806c255373198ae1d3d754c
mnhegde <54085487+mnhegde@users.noreply.github.com>
2020-05-31 22:47:56 -0400

    db stuff

--- main.py
+++ main.py
@@ -25,17 +25,21 @@ class User(db.Model):
     gender = db.Column(db.String(50))
     currentWeight = db.Column(db.Integer)
     goalWeight = db.Column(db.Integer)
-    date_created = db.Column(db.DateTime, default=datetime.now)
+    date_created = db.Column(db.DateTime, default=datetime.now())
 
 
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
     name = db.Column(db.String(50))
+    image = db.Column(db.String(1000))
     serving = db.Column(db.Integer)
     calories = db.Column(db.Integer)
-    macromolecules = db.Column(db.String(50))
-    date_created = db.Column(db.DateTime, default=datetime.now)
+    protein = db.Column(db.Integer)
+    fat = db.Column(db.Integer)
+    carbs = db.Column(db.Integer)
+    section = db.Column(db.String(50))
+    date_created = db.Column(db.DateTime, default=datetime.now())
 
 class Exercise(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,7 +48,25 @@ class Exercise(db.Model):
     sets = db.Column(db.Integer)
     reps = db.Column(db.Integer)
     time = db.Column(db.Integer)
-    date_created = db.Column(db.DateTime, default=datetime.now)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+
+
+def getFoods():
+    current_user = db.session.query(User).filter(User.username==session['username']).first()
+    foods = db.session.query(Food).filter(Food.user_id==current_user.id).all()
+    food_info = {}
+    for i in range(len(foods)):
+        food_info['Food %s' %(i+1)] = [foods[i].id, foods[i].name.title(), foods[i].image, foods[i].serving, foods[i].calories, foods[i].protein, foods[i].fat, foods[i].carbs, foods[i].section, str(foods[i].date_created.year) + '-' + str(foods[i].date_created.month) + '-' + str(foods[i].date_created.day)]
+    return json.dumps(food_info)
+
+
+def getExercises():
+    current_user = db.session.query(User).filter(User.username==session['username'])
+    exercises = db.session.query(Food).filter(Exercise.user_id==current_user.id).all()
+    exercise_info = {}
+    for i in range(len(exercises)):
+        exercise_info['Exercise %s' %(i+1)] = [exercises[i].id, exercises[i].name.title(), exercises[i].sets, exercises[i].reps, exercises[i].time, exercises[i].date_created]
+    return exercise_info
 
 
 @app.before_request
@@ -86,6 +108,7 @@ def login():
             flash('The user doesn\'t exist or the password provided was incorrect', 'error')
             return redirect('/login')   
     else:
+        print(datetime.now())
         return render_template('login.html')
 
 @app.route('/signup', methods = ['GET', 'POST'])
@@ -98,13 +121,13 @@ def signup():
         confirmPassword = data['confirmPassword']
         if password == confirmPassword:
             matchingUsernames = False
-            #matchingUsernames = db.session.query(User).filter(User.username==username).first()
+            matchingUsernames = db.session.query(User).filter(User.username==username).first()
             if matchingUsernames:
                 flash('Username already exists!', 'error')
                 return redirect('/signup')
             else:
                 matchingEmail = False
-                #matchingEmail = db.session.query(User).filter(User.email==email).first()
+                matchingEmail = db.session.query(User).filter(User.email==email).first()
                 if matchingEmail:
                     flash('Email is already registered with another account!', 'error')
                     return redirect('/signup')
@@ -119,8 +142,8 @@ def signup():
                     user = User(username=username, email=email, firstname=firstname, lastname=lastname, 
                     password = makePasswordHash(password), height=height, age=age, gender=gender, currentWeight=currentWeight,
                     goalWeight=goalWeight)
-                    #db.session.add(user)
-                    #db.session.commit()
+                    db.session.add(user)
+                    db.session.commit()
 
                     session['username'] = username
                     session['firstname'] = firstname
@@ -146,19 +169,37 @@ def credits():
     return render_template('resources.html')
 
 
-@app.route('/exercise')
-def exersise():
-    return render_template('exersise.html')
+@app.route('/exercise', methods=['GET', 'POST'])
+def exercise():
+    if request.method == 'POST':
+        request_data = request.json
+        data = request_data['data']
+        user = db.session.query(User).filter(User.username == session['username']).first()
+        exercise = Exercise(user_id=user.id, name=data['exercise'], time=data['time'], sets=data['numSets'], reps=data['numReps'])
+        db.session.add(exercise)
+        db.session.commit()
+    else:
+        return render_template('exercise.html')
 
-@app.route('/food')
+@app.route('/food', methods=['GET', 'POST'])
 def food():
-    return render_template('food.html')
+    if request.method == 'POST':
+        data = request.json
+        user = db.session.query(User).filter(User.username == session['username']).first()
+        food = Food(user_id=user.id, name=data['name'], image=data['image'], serving=data['serving'], calories=data['calories'], protein=data['protein'], 
+            fat=data['fat'], carbs=data['carbs'], section=data['section'])
+        db.session.add(food)
+        db.session.commit()
+        addedFood = db.session.query(Food).filter(Food.name==data['name']).all()
+        return json.dumps(addedFood[-1].id)
+    else:
+        return render_template('food.html')
 
 @app.route('/run')
 def run():
     return render_template('run.html')
 
-@app.route('/api/exersises')
+@app.route('/api/exercises')
 def exerises():
     with open ('data/exersises.json','r') as read_file:
         return read_file.read()
@@ -170,6 +211,38 @@ def test():
 @app.route('/analytics')
 def analytics():
     return render_template('analytics.html')
+
+
+@app.route('/api/food')
+def foodSearch():
+    foods = getFoods()
+    return foods
+
+@app.route('/api/exercise')
+def exerciseSearch():
+    exercises = getExercises()
+    return exercises
+
+@app.route('/api/foodedit', methods=['GET', 'POST'])
+def foodEdit():
+    if request.method == 'POST':
+        data = request.json
+        db.session.query(Food).filter(Food.id==data['id']).\
+            update({'serving': data['serving'], 'calories': data['calories'], 'protein': data['protein'], 'fat': data['fat'], 'carbs': data['carbs']})
+        db.session.commit()
+
+
+@app.route('/api/fooddelete', methods=['GET', 'POST'])
+def fooddelete():
+    if request.method == 'POST':
+        data = request.json
+        print(data)
+        food = db.session.query(Food).filter(Food.id==data).first()
+        db.session.delete(food)
+        db.session.commit()
+    
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
